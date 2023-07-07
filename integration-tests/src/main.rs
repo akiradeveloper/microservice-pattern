c86c9fc9bb97540c65690f3cd6d398f467099794
Akira Hayakawa <ruby.wktk@gmail.com>
2023-07-07 10:34:30 +0900

    Update test

--- integration-tests/src/main.rs
+++ integration-tests/src/main.rs
@@ -24,7 +24,7 @@ struct AppImpl {
 #[microservice_pattern::service_impl]
 impl MyApp for AppImpl {
     async fn c(&self) -> u32 {
-        0
+        self.cli.a("hello").await
     }
 }
 
@@ -39,11 +39,15 @@ mod tests {
         assert_eq!(cli.a("hello").await, 0);
         assert_eq!(cli.b(1).await, None);
 
+        let app = MyAppServer::new(AppImpl { cli: cli.clone() });
+        assert_eq!(app.c().await, 0);
+
         // mockall works
         let mut mock = MockMyService::new();
         mock.expect_a().return_const(1u32);
-        assert_eq!(mock.a("hello").await, 1);
         mock.expect_b().return_const(Some(1));
+        let mock = MyServiceServer::new(mock);
+        assert_eq!(mock.a("hello").await, 1);
         assert_eq!(mock.b(1).await, Some(1));
     }
 }
